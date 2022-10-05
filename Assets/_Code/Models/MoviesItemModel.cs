4c534abd6dd70d0e41ac0c9975ab323ab166b738
Rojo5Amerike <rojo5@amerike.edu.mx>
2022-10-05 18:51:15 -0500

    Movies Model

---
+++ Assets/_Code/Models/MoviesItemModel.cs
@@ -0,0 +1,47 @@
+using System;
+
+[Serializable]
+public class MoviesItemModel 
+{
+    public int Id;
+    public string Name;
+    public int Year;
+    public string Director;
+    public string LeadActor;
+    public string SupportActor;
+    public float CriticScore;
+    public float AudienceScore;
+    public string Synopsis;
+    public double Budget;
+    public double Revenue;
+
+
+    public MoviesItemModel(string name, int year, string director, string leadActor, string supportActor, float criticScore, float audienceScore, string synopsis, double budget, double revenue)
+    {
+        Name = name;
+        Year = year;
+        Director = director;
+        LeadActor = leadActor;
+        SupportActor = supportActor;
+        CriticScore = criticScore;
+        AudienceScore = audienceScore;
+        Synopsis = synopsis;
+        Budget = budget;
+        Revenue = revenue;
+    }
+
+    public MoviesItemModel(int id, string name, int year, string director, string leadActor, string supportActor, float criticScore, float audienceScore, string synopsis, double budget, double revenue)
+    {
+        Id = id;
+        Name = name;
+        Year = year;
+        Director = director;
+        LeadActor = leadActor;
+        SupportActor = supportActor;
+        CriticScore = criticScore;
+        AudienceScore = audienceScore;
+        Synopsis = synopsis;
+        Budget = budget;
+        Revenue = revenue;
+    }
+}
--- Assets/_Code/RequestController.cs
+++ Assets/_Code/RequestController.cs
@@ -9,11 +9,20 @@ public class RequestController : MonoBehaviour
     [SerializeField] private MyAPIRequest.RequestType _RequestType;
 
     [Header("Settings")]
-    [SerializeField] private int _modelID;
-    [SerializeField] private string _modelName;
-    [SerializeField] private bool _modelIsComplete;
+    [SerializeField] private int _id;
+    [SerializeField] private string _name;
+    [SerializeField] private int _year;
+    [SerializeField] private string _director;
+    [SerializeField] private string _leadActor;
+    [SerializeField] private string _supportActor;
+    [SerializeField] private float _criticScore;
+    [SerializeField] private float _audienceScore;
+    [TextArea(5,15)]
+    [SerializeField] private string _synopsis;
+    [SerializeField] private double _budget;
+    [SerializeField] private double _revenue;
 
-    private TodoItemModel _myModel;
+    private MoviesItemModel _myModel;
 
     private void Start()
     {
@@ -24,13 +33,13 @@ public class RequestController : MonoBehaviour
     {
         BuildModel();
         _APIRequester.RequestTypeOf = _RequestType;
-        _APIRequester.SendRequest(_myModel, _myModel.TodoItemId);
+        _APIRequester.SendRequest(_myModel, _myModel.Id);
     }
 
     private void BuildModel()
     {
-        if (_modelID > 0) _myModel = new TodoItemModel(_modelID, _modelName, _modelIsComplete);
-        else _myModel = new TodoItemModel(_modelName, _modelIsComplete);
+        if (_id > 0) _myModel = new MoviesItemModel(_id, _name, _year, _director, _leadActor, _supportActor, _criticScore, _audienceScore, _synopsis, _budget, _revenue);
+        else _myModel = new MoviesItemModel(_name, _year, _director, _leadActor, _supportActor, _criticScore, _audienceScore, _synopsis, _budget, _revenue);
     }
 
     private void Prepare()
